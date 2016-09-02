2945ae29e1be96ca1bfd81cecf51e5562052acc6
bruno <bruno.belmondo@gmail.com>
2016-09-02 18:13:25 +0200

    launched child process to execute code

--- CodeTrials/routes/codeValidation.js
+++ CodeTrials/routes/codeValidation.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var exec = require('child_process').exec;
+
 router.get('/*', function(req, res, next) {
-    var answer = { data: 'yes I received it' };
-    res.type('application/json');
-    res.send(answer);
-    res.end();
+    var answer = { data: 'nothing' };
+    var testParameters = '[1,2,3]';
+    var testCode = req.param('code');
+    console.log('received code :' + testCode);
+    //JSON.stringify('var solution = function(numbers) {return Math.max.apply(null, numbers);};exports.solution = solution;');
+
+    exec('../js_solver/launch ' + testCode + ' ' + testParameters, function(error, stdout, stderr) {
+        console.log('stdout: ', stdout);
+        console.log('stderr: ', stderr);
+        answer = { data: stdout };
+        if (error !== null) {
+            console.log('exec error: ', error);
+            answer = { data: stderr };
+        }
+
+        res.type('application/json');
+        res.send(answer);
+        res.end();
+    });
 });
 
 module.exports = router;
